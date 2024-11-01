6fe45b3513491bb98c763e64f7184dde2576e41e
Adam McKenna <adam-mckenna@outlook.com>
2024-11-01 13:24:01 +0700

    feat: refactored nav. Using IntersectionObserver API to highlight the relevant nav section.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,20 +1,41 @@
 import React, { useEffect, useState } from "react";
 import hasScrolledToTop from "../helpers/hasScrolledToTop";
 
-const Links: { [key: string]: any } = {
-  About: "top",
-  Experience: "experience",
-  Portfolio: "portfolio",
-  "Career Breaks": "career-breaks",
+const Links: { [key: string]: string } = {
+  // Targetting the '#top' as this will take the user back to the top of the page,
+  // instead of slightly below if we just used the '#about' anchor.
+  top: "About",
+  experience: "Experience",
+  portfolio: "Portfolio",
+  "career-breaks": "Career Breaks",
 };
 
-// todo: give this a SERIOUS refactor
+const ObserverOptions = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.1,
+};
+
 const Nav = () => {
-  const [visibleElements, setVisibleElements] = useState(new Set());
+  const [visibleSections, setVisibleSections] = useState(new Set());
 
   const [currentView, setCurrentView] = useState("top");
 
   const isAtTop = hasScrolledToTop();
+
+  const ObserverCallback: IntersectionObserverCallback = (entries) => {
+    entries.forEach(({ isIntersecting, target }) =>
+      setVisibleSections(
+        isIntersecting
+          ? (prev) => new Set(prev).add(target)
+          : (prev) => {
+              const newSet = new Set(prev);
+              newSet.delete(target);
+              return newSet;
+            },
+      ),
+    );
+  };
 
   useEffect(() => {
     if (isAtTop) {
@@ -23,59 +44,34 @@ const Nav = () => {
   }, [isAtTop]);
 
   useEffect(() => {
-    // Select the external element outside React
-    const targetElements = [
+    const navLinks = [
       document.getElementById("about"),
       document.getElementById("experience"),
       document.getElementById("portfolio"),
       document.getElementById("career-breaks"),
     ];
 
-    // Create the IntersectionObserver
     const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            // Add to the visible elements set
-            setVisibleElements((prev) => new Set(prev).add(entry.target));
-          } else {
-            // Remove from the visible elements set
-            setVisibleElements((prev) => {
-              const newSet = new Set(prev);
-              newSet.delete(entry.target);
-              return newSet;
-            });
-          }
-        });
-      },
-      {
-        root: null,
-        rootMargin: "0px",
-        threshold: 0.1,
-      },
+      ObserverCallback,
+      ObserverOptions,
     );
 
-    // Observe each element
-    targetElements.forEach((el) => observer.observe(el as HTMLElement));
+    navLinks.forEach((element) => observer.observe(element as HTMLElement));
 
-    // Cleanup observer on unmount
-    return () => {
-      targetElements.forEach((el) => observer.unobserve(el as HTMLElement));
-    };
+    return () =>
+      navLinks.forEach((element) => observer.unobserve(element as HTMLElement));
   }, []);
 
   useEffect(() => {
-    if (
-      visibleElements.entries().next().value &&
-      visibleElements.entries().next().value[0].id
-    ) {
-      setCurrentView(
-        visibleElements.entries().next().value[0].id === "about"
-          ? "top"
-          : visibleElements.entries().next().value[0].id,
-      );
+    const elementsInView = visibleSections.entries().next().value;
+    const latestElement = elementsInView && (elementsInView[0] as HTMLElement);
+
+    if (latestElement) {
+      const targetAnchor =
+        latestElement.id === "about" ? "top" : latestElement.id;
+      setCurrentView(targetAnchor);
     }
-  }, [visibleElements]);
+  }, [visibleSections]);
 
   return (
     <nav className="max-lg:hidden lg:mt-12">
@@ -83,10 +79,10 @@ const Nav = () => {
         {Object.keys(Links).map((link, i) => (
           <li key={i}>
             <a
-              className={`${i >= 1 ? "mt-4" : ""} ${currentView === Links[link] ? "" : "opacity-50"} block text-xl font-bold uppercase hover:opacity-100 focus:opacity-100`}
+              className={`${i >= 1 ? "mt-4" : ""} ${currentView === link ? "" : "opacity-50"} block text-xl font-bold uppercase hover:opacity-100 focus:opacity-100`}
               href={`#${Links[link]}`}
             >
-              {link}
+              {Links[link]}
             </a>
           </li>
         ))}
