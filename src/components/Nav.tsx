47c34d88aa8d20c2983c509291e210208f0c9acf
Adam McKenna <adam-mckenna@outlook.com>
2025-06-20 20:36:07 +0200

    feat: animated underline

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import hasScrolledToTop from "../helpers/hasScrolledToTop";
 
 const Links: { [key: string]: string } = {
